067d1b0ede46b756b24db6a8fda8aa88d4fe2749
Roberto Reyes <robertoreyes9510@gmail.com>
2018-02-08 17:33:07 -0600

    otro comentario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
                 <div class="title m-b-md">
                     Roberto Reyes
                 </div>
-
+<!--cambios-->
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
