4772242989220d4a7163e130c944e2def1a77949
beto <robertoc-95@hotmail.com>
2018-02-08 17:25:22 -0600

    segundo comentario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Roberto Reyes
                 </div>
 
                 <div class="links">
