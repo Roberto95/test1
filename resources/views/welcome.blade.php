c73df44706780488c29ce3f83249f9d95855697e
beto <robertoc-95@hotmail.com>
2018-02-08 18:27:33 -0600

    prueba de env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
                 <div class="title m-b-md">
                     Roberto Reyes
                 </div>
-<!--cambios desde pc de escritorio-->
+<!--cambios desde pc de escritorio cambio hecho de nuevo-->
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
